bcb69f9980ce3c26458261b8e63f143c46aa893b
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-06-01 16:43:37 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # challenge-dataone
+The program takes a csv file, and a list of products that someone wants to buy, and outputs the shop they should go to, and the total price it will cost them. It is okay to purchase extra products, as long as the total cost is minimized. The program assumes that the customer wants to buy all the products from a single shop.
+
+Shop IDs are integers, all products are lower case letters and underscores, and the price is a decimal number.
+
+Sample Data set:
+
+Data File data.csv
+
+1, 4.00, teddy_bear
+1, 8.00, baby_powder
+2, 5.00, teddy_bear
+2, 6.50, baby_powder
+3, 4.00, pampers_diapers
+3, 8.00, johnson_wipes
+4, 5.00, johnson_wipes
+4, 2.50, cotton_buds
+5, 4.00, bath_towel
+5, 8.00, scissor
+6, 5.00, scissor
+6, 6.00, bath_towel, cotton_balls, powder_puff
+
+Usage instructions:
+1. [Download Python 3.6.1](https://www.python.org/ftp/python/3.6.1/python-3.6.1.exe)
+2. Clone this repo to desired location.
+3. Run the file expense.py
---
+++ expense.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 21:01:30 2017
+
+@author: Mayank Shekhar
+"""
+
+
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading csv file...")
+    inFile = open('data.csv', 'r')
+    shopList = []
+    for line in inFile:
+        shopList.append(line.strip().split(', '))
+    return shopList
+ 
+def expenseCal(shopList):
+    for i in shopList:
+        print(i)
+    items = []
+    pmatch = []
+    pmatch1 = []
+    items.extend(input('Enter the items:').split(' '))
+    
+    #generating a list of available items details
+    for i in items:
+        for j in shopList:
+            if i in j:
+                pmatch1.extend([i])
+                pmatch.append([j[0],j[1],i])
+    
+    #if all the items are not found print none
+    for i in items:
+        if i not in pmatch1:
+            print('none')
+            input('Enter any key to exit')
+            return 1
+    
+    #generating possible shops
+    shops = []
+    for i in pmatch:
+        if i[0] not in shops:
+            shops.extend(i[0])
+    
+    #removing shops which don't have all the items
+    pmatch2 = pmatch[:]
+    shops1 = shops[:]
+    for i in shops1:
+        count = 0
+        for j in items:
+            for k in pmatch2:
+                if i == k[0] and j == k[2]:
+                    count+=1
+        if count != len(items):
+            for l in pmatch:
+                if l[0] == i:
+                    pmatch.remove(l)
+                    shops.remove(l[0])
+    if not pmatch:
+        print('none')
+        input('Enter any key to exit')
+        return 1
+        
+    #removing rendundant combo items
+    pmatch3 = pmatch[:]
+    for i in range(0, len(pmatch3)):
+        for j in pmatch3[i+1:]:
+            if pmatch3[i][0] == j[0] and pmatch3[i][1] == j[1]:
+                pmatch.remove(j)
+    if not pmatch:
+        print('none')
+        input('Enter any key to exit')
+        return 1
+        
+    #calculating the total cost per shop
+    tcost = {}
+    for i in shops:
+        cost = 0
+        for j in pmatch:
+            if i == j[0]:
+                cost += float(j[1])
+        tcost[i] = cost
+        
+    #choosing the shop providing items at the cheapest price
+    min = list(tcost.values())[0]
+    shop = list(tcost.keys())[0]
+    for i in list(tcost.keys()):
+        if min > tcost[i]:
+            min = tcost[i]
+            shop = i
+    
+    print(shop+', ', min)
+    input('Enter any key to exit')
+        
+    
+    
+    
+    
+expenseCal(loadWords())
+                
+        
